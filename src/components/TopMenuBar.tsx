38659bc5fcc2f0f8c988fd4ad1ca73c3ec36442a
JNaeder <J.naeder324@gmail.com>
2025-09-17 10:55:57 -0400

    Added redux, created basic items, tested it with a throw away thing

--- src/components/TopMenuBar.tsx
+++ src/components/TopMenuBar.tsx
@@ -3,6 +3,9 @@ import type { User } from "@/types";
 import type { Auth } from "firebase/auth";
 import BoxPoolLogo from "../assets/boxpool-logo-1.svg";
 import { NavLink } from "react-router";
+import { Button } from "./ui/button";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { plusOne, setValue } from "@/slices/testSlice";
 
 export default function TopMenuBar({
   auth,
@@ -13,6 +16,9 @@ export default function TopMenuBar({
   currentUser: User | null;
   setCurrentUser: Function;
 }) {
+  const numberValue = useAppSelector((s) => s.test);
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <div className="bg-black text-white flex justify-between items-center gap-5 h-[50px] px-10">
@@ -22,6 +28,23 @@ export default function TopMenuBar({
             <div className="text-4xl w-full ">boxpool.live</div>
           </div>
         </NavLink>
+        <div className="flex items-center justify-center space-x-5">
+          <Button
+            onClick={() => {
+              dispatch(setValue(69));
+            }}
+          >
+            Set to 69
+          </Button>
+          <Button
+            onClick={() => {
+              dispatch(plusOne());
+            }}
+          >
+            Increase
+          </Button>
+          <div>{numberValue}</div>
+        </div>
         <AuthButton
           currentUser={currentUser}
           setCurrentUser={setCurrentUser}
